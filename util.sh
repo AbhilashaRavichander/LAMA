d39af1daa22d062000566ef5c7006f4bc7374ed6
jzb <zhengbaj@cs.cmu.edu>
2019-10-30 17:10:25 -0400

    update

--- util.sh
+++ util.sh
@@ -2,11 +2,12 @@
 
 eval() {
     # | grep -P '^first [0-9].+' | awk -F'[ ,]' '{print $2}'
+    # | awk '{sum += $1} END {print "mean = " sum/NR}'
     set -e
     for file in data/TREx/*; do
         bfile=$(basename "${file}")
         outfile=${1}/${bfile}.out
-        objfile=output/relational_phrase_exp/trex_objs/${bfile}.out
+        objfile=output/exp/trex_objs/${bfile}.out
         echo ${bfile}
         if [ -f "${outfile}" ]; then
             if [ $# -gt 1 ]; then
