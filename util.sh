7f7b2cca9015e3e73650b65f83d4ce55c8b5a25d
jzb <zhengbaj@cs.cmu.edu>
2019-10-30 00:23:26 -0400

    add util script

---
+++ util.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+eval() {
+    # | grep -P '^first [0-9].+' | awk -F'[ ,]' '{print $2}'
+    set -e
+    for file in data/TREx/*; do
+        bfile=$(basename "${file}")
+        outfile=${1}/${bfile}.out
+        objfile=output/relational_phrase_exp/trex_objs/${bfile}.out
+        echo ${bfile}
+        if [ -f "${outfile}" ]; then
+            if [ $# -gt 1 ]; then
+                python scripts/ana.py --task out --inp ${outfile} --obj_file ${objfile}
+            else
+                python scripts/ana.py --task out --inp ${outfile}
+            fi
+        fi
+    done
+}
+
+if [[ $1 == 'eval' ]]; then
+    eval $2 $3
+elif [[ $1 == 'other' ]]; then
+    echo other
+fi
